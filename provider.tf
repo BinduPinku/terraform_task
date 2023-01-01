eccee61f46f59f84ab2cc70fa0e2825c4f700a97
Bindu_S <115168943+BinduPinku@users.noreply.github.com>
2023-01-01 21:46:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,4 +13,6 @@ provider "azurerm" {
   features {}
   subscription_id   = "a6815a44-28cb-4bac-850c-d74430761c76"
   tenant_id       = "fcf6883e-631a-408b-8b42-e3310b0ee03a"
+  client_id       = "e5fd2d48-a242-4b8d-97b0-3880f57c7476"
+  client_secret   = "6G18Q~YHOS86Kp9ruu95DJwvyHKUBiOxU61sUb4z"
 }
